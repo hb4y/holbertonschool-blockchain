07badc9fcdf2ad355d58c034489071c20f0a1e31
hb4y <hugobaymon@gmail.com>
2021-12-12 00:55:36 -0500

    save key pairs

---
+++ crypto/ec_save.c
@@ -0,0 +1,43 @@
+#include "hblk_crypto.h"
+
+/**
+* ec_save - save on file
+* @key: key
+* @folder: folder
+* Return: 1
+*/
+int ec_save(EC_KEY *key, char const *folder)
+{
+	FILE *descriptor;
+	char c_dir[256];
+
+	if (key == NULL || folder == NULL)
+		return (0);
+
+	mkdir(folder, S_IRWXU | S_IRWXG | S_IROTH | S_IXOTH);
+	sprintf(c_dir, "%s/%s", folder, "key.pem");
+	descriptor = fopen(c_dir, "w");
+	if (!descriptor)
+		return (0);
+	if (!PEM_write_ECPrivateKey(descriptor, key, NULL, NULL, 0, NULL, NULL))
+	{
+		fclose(descriptor);
+		return (0);
+	}
+	fclose(descriptor);
+	sprintf(c_dir, "%s/%s", folder, "key_pub.pem");
+	descriptor = fopen(c_dir, "w");
+	if (!descriptor)
+	{
+		return (0);
+	}
+	if (!PEM_write_EC_PUBKEY(descriptor, key))
+	{
+		fclose(descriptor);
+
+		return (0);
+	}
+	fclose(descriptor);
+
+	return (1);
+}
