cec17a7ccfc1fc533a0cd7bd00045a10e80558d4
hb4y <hugobaymon@gmail.com>
2021-12-12 01:45:38 -0500

    fix betty

--- crypto/test/ec_verify-main.c
+++ crypto/test/ec_verify-main.c
@@ -8,9 +8,9 @@ void _print_hex_buffer(uint8_t const *buf, size_t len);
 
 /**
  * test_ec_sign - Test the ec_sign function
- * 
+ *
  * @key: Pointer to the EC Key pair to use to sign the message
- * 
+ *
  * Return: EXIT_SUCCESS or EXIT_FAILURE
  */
 static int test_ec_sign(EC_KEY const *key)
