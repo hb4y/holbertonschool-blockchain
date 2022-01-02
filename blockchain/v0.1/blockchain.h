0e70af1b30d57c7704ffb3cfb552505e3e9d5ad1
hb4y <hugobaymon@gmail.com>
2022-01-02 15:32:31 -0500

    Initial commit and basic headers

---
+++ blockchain/v0.1/blockchain.h
@@ -0,0 +1,15 @@
+#ifndef BLOCK_CHAIN_H
+#define BLOCK_CHAIN_H
+
+# include "../../crypto/hblk_crypto.h"
+# include <llist.h>
+# include <stdint.h>
+# include <openssl/sha.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include <time.h>
+
+
+
+#endif
