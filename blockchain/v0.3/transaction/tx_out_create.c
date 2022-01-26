e1e324c4df156e8f8143e4cfd198635a1ffb3f8f
hb4y <hugobaymon@gmail.com>
2022-01-25 22:57:52 -0500

    betty

--- blockchain/v0.3/transaction/tx_out_create.c
+++ blockchain/v0.3/transaction/tx_out_create.c
@@ -16,7 +16,8 @@ tx_out_t *tx_out_create(uint32_t amount, uint8_t const pub[EC_PUB_LEN])
 	tx_n->amount = amount;
 
 	memcpy(tx_n->pub, pub, sizeof(tx_n->pub));
-	if (!sha256((int8_t const *)tx_n, sizeof(tx_n->amount) + sizeof(tx_n->pub), tx_n->hash))
+	if (!sha256((int8_t const *)tx_n,
+		sizeof(tx_n->amount) + sizeof(tx_n->pub), tx_n->hash))
 		return (free(tx_n), NULL);
 
 	return (tx_n);
